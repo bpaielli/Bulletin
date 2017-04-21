5a81f575052aba35622457187526405279e84b9d
Brittany Paielli <bmpaielli@gmail.com>
2017-04-21 11:52:29 -0700

    Added error handling for account register backend work

--- assets/scripts/accountregister.php
+++ assets/scripts/accountregister.php
@@ -1,1 +1,30 @@
+<!-- 
+
+This file will get user info,
+salt the password,
+and make a new row in the database.
+
+-->
+
+
 <?php
+
+// Error Handling for empty field
+if(empty($_POST['firstname'])){
+	$this->HandleError("ERROR: First Name Field is empty");
+	return false;
+}else if(empty($_POST['lastname'])){
+	$this->HandleError("ERROR: Last Name Field is empty");
+	return false;
+}else if(empty($_POST['email'])){
+	$this->HandleError("ERROR: Email Field is empty");
+	return false;
+}else if(empty($_POST['password'])){
+	$this->HandleError("ERROR: Password Field is empty");
+	return false;
+}
+
+
+
+
+?>
