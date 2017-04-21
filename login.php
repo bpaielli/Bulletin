e1eed28e48e1d8239da63836ff8b0ba126f6add0
Brittany Paielli <bmpaielli@gmail.com>
2017-04-21 10:03:39 -0700

    Splash is complete, login and register front end complete

---
+++ login.php
@@ -0,0 +1,27 @@
+<!-- Programmers: Austin Mutschler & Brittany Paielli -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Bulletin - Login</title>
+<link href="assets/style/start.css" type="text/css" rel="stylesheet">
+<link rel="icon" type="image/x-icon" href="favicon.ico">
+<link href="https://fonts.googleapis.com/css?family=Titillium+Web:300" rel="stylesheet">
+</head>
+<body>
+	<div id="overlay">
+		<div id="logincontainer">
+			<p id="loginheader">Bulletin</p>
+			<form id="formblock" action="login.php" method="post">
+				<input type="email" name="email" placeholder="Email" autofocus
+					required> <input type="password" name="password"
+					placeholder="Password" required>
+
+				<p>
+					<input class="mainbuttons" type="submit" value="Login"> <a class="elsetext"> or </a> <a class="orhyper" href="register.php">Register</a>
+				</p>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,30 @@
+<!-- Programmers: Austin Mutschler & Brittany Paielli -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Bulletin - Register</title>
+<link href="assets/style/start.css" type="text/css" rel="stylesheet">
+<link rel="icon" type="image/x-icon" href="favicon.ico">
+<link href="https://fonts.googleapis.com/css?family=Titillium+Web:300" rel="stylesheet">
+
+</head>
+<body>
+	<div id="overlay">
+		<div id="registercontainer">
+			<p id="registerheader">Bulletin</p>
+			<form id="formblock" action="register.php" method="post">
+				<input type="text" name="firstname" placeholder="First Name"
+					autofocus required> <input type="text" name="lastname"
+					placeholder="Last Name" required> <input type="email" name="email"
+					placeholder="Email" required> <input type="password"
+					name="password" placeholder="Password" required>
+
+				<p>
+					<input class="mainbuttons" type="submit" value="Register"> <a class="elsetext"> or </a> <a class="orhyper" href="login.php">Login</a>
+				</p>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
