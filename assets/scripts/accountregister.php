98097162bd8f4d3cd9e510bbfd3401e471305d02
Brittany Paielli <bmpaielli@gmail.com>
2017-04-22 10:19:20 -0700

    added comments

--- assets/scripts/accountregister.php
+++ assets/scripts/accountregister.php
@@ -1,6 +1,6 @@
 <!-- 
 
-This file will get user info,
+This file will retrieve user info,
 salt the password,
 and make a new row in the database.
 
@@ -9,22 +9,40 @@ and make a new row in the database.
 
 <?php
 
-// Error Handling for empty field
-if(empty($_POST['firstname'])){
-	$this->HandleError("ERROR: First Name Field is empty");
-	return false;
-}else if(empty($_POST['lastname'])){
-	$this->HandleError("ERROR: Last Name Field is empty");
-	return false;
-}else if(empty($_POST['email'])){
-	$this->HandleError("ERROR: Email Field is empty");
-	return false;
-}else if(empty($_POST['password'])){
-	$this->HandleError("ERROR: Password Field is empty");
-	return false;
+class accountRegister{
+	
+	
+function createAccount(){
+	// Error Handling for empty field
+	if(empty($_POST['firstname'])){
+		$this->HandleError("ERROR: First Name Field is empty");
+		return false;
+	}else if(empty($_POST['lastname'])){
+		$this->HandleError("ERROR: Last Name Field is empty");
+		return false;
+	}else if(empty($_POST['email'])){
+		$this->HandleError("ERROR: Email Field is empty");
+		return false;
+	}else if(empty($_POST['password'])){
+		$this->HandleError("ERROR: Password Field is empty");
+		return false;
+	}
+	
+	//trim and set all variables
+	$firstname = trim($_POST['firstname']);
+	$lastname = trim($_POST['lastname']);
+	$email = trim($_POST['email']);
+	$password = trim($_POST['password']);
+	
+	//Check if the user exists in database
+	
+	
+	
+	//Add user into database
+	
+	
 }
 
 
-
-
+} // end class accountRegister
 ?>
